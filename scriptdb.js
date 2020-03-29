b4df4d9c47585c34fb6d6e05e0bdf67e4f4667f5
Habby Lovo <herberth.jc@gmail.com>
2020-03-28 22:52:42 -0600

    prueba

---
+++ scriptdb.js
@@ -0,0 +1,71 @@
+if ('serviceWorker' in navigator) {	
+    navigator.serviceWorker.register('sw.js')
+    .then(reg => console.log('Registro de Service Worker exitoso', reg))
+    .catch(err => console.warn('Error al tratar de registrar el Service Worker', err))
+}
+
+const indexed = indexedDB
+const form = document.getElementById('formulario')
+if(indexed && form){
+    let db
+    const request = indexed.open('base', 1)
+    request.onsuccess = () =>{
+        db = request.result
+        console.log('OPEN', db)
+        readData()
+    }
+    request.onupgradeneeded = () =>{
+        db = request.result
+        console.log('CREATE', db)
+        const objectStore = db.createObjectStore('tabla',{ autoIncrement : true })
+    }
+    request.onerror = (error) => {
+        console.log('Error', error)
+    }
+
+    const addData = (data) => {
+        const transaction = db.transaction(['tabla'], 'readwrite')
+        const objectStore = transaction.objectStore('tabla')
+        const request = objectStore.add(data)
+    }
+    const readData = () => {
+        const transaction = db.transaction(['tabla'], 'readonly')
+        const objectStore = transaction.objectStore('tabla')
+        const request = objectStore.openCursor()
+        const fragment = document.createDocumentFragment()
+        request.onsuccess = (e) =>{
+            const cursor = e.target.result
+            if(cursor){
+                const tr = document.createElement('tr')
+                const nombre = document.createElement('td')
+                nombre.textContent = cursor.value.nombre
+                const apellido = document.createElement('td')
+                apellido.textContent = cursor.value.apellido
+
+                tr.insertCell().textContent = cursor.value.nombre
+                tr.insertCell().textContent = cursor.value.apellido
+
+                fragment.appendChild(tr)
+                console.log(cursor.value)
+                cursor.continue()
+            } else{
+                $("#tabla").html(fragment);
+                // tabla.textContent = ''
+                // tabla.appendChild(fragment)
+            }
+        }
+    }
+
+    form.addEventListener('submit', (e) => {
+        e.preventDefault()
+        const data = {
+            nombre:e.target.nombre.value,
+            apellido:e.target.apellido.value
+        }
+        addData(data);
+        e.target.nombre.value = ''
+        e.target.apellido.value = ''
+        readData()
+        console.log(data)
+    })
+}
---
+++ sw.js
@@ -0,0 +1,58 @@
+    //Asignar un nombre y versión al cache
+    const CACHE_NAME = 'cache_prueba'
+    const cacheDinamico = 'cache_prueba2'
+    const urlsToCache = [  
+        '/',
+        './',
+        './index.html',
+        // 'https://fonts.googleapis.com/css?family=Montserrat:400,500|Poppins:400,500,600,700|Roboto:400,500',
+        // 'https://cdn.materialdesignicons.com/4.4.95/css/materialdesignicons.min.css',
+        // 'https://unpkg.com/sleek-dashboard/dist/assets/css/sleek.min.css',
+        'https://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js',
+        'http://localhost/IndexedDb/assets/template/dist/assets/img/logo_cash.png',
+        'manifest.json',
+        'scriptdb.js'
+    ]
+
+//durante la fase de instalación, generalmente se almacena en caché los activos estáticos
+self.addEventListener('install', e => {
+
+    e.waitUntil(
+        caches.open(CACHE_NAME).then(cache => {
+            console.log("cache")
+            cache.addAll(urlsToCache)
+        })
+        .catch(err => console.log('Falló registro de cache', err))
+    )
+})
+
+//una vez que se instala el SW, se activa y busca los recursos para hacer que funcione sin conexión
+self.addEventListener('activate', e => {
+    e.waitUntil(
+        caches.keys().then(keys => {
+            return Promise.all(
+                keys.filter(key => key !== CACHE_NAME)
+                .map(key => caches.delete(key)) //Eliminamos lo que ya no se necesita en cache
+            )
+        })
+        // Le indica al SW activar el cache actual
+        .then(() => self.clients.claim())
+        .catch(err => console.log("Fallo al activar"))
+    )
+})
+
+//cuando el navegador recupera una url
+self.addEventListener('fetch', e => {
+    //Responder ya sea con el objeto en caché o continuar y buscar la url real
+    e.respondWith(
+        caches.match(e.request).then(cacheRes => {
+            //recuperando cache
+            return cacheRes || fetch(e.request).then(fetchRes => {
+                return caches.open(cacheDinamico).then(cache => {
+                    cache.put(e.request.url, fetchRes.clone())
+                    return fetchRes;
+                })
+            })
+        })
+    )
+})
